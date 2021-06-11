082faa1ab0a8594664a499cb0781a0fe271a76a8
Benjamin Delespierre <benjamin.delespierre@gmail.com>
2021-06-11 13:46:21 +0200

    [5] update MyString::append for metric byte units

--- src/MyString.php
+++ src/MyString.php
@@ -16,6 +16,33 @@ class MyString
 
     public function append(string $suffix): void
     {
+        if ($this->isMetricByteUnit($suffix)) {
+            $suffix = " " . $this->sanitizeMetricByteUnit($suffix);
+        }
+
         $this->value .= $suffix;
+    }
+
+    protected function isMetricByteUnit(string $suffix): bool
+    {
+        // 1000     kB  kilobyte
+        // 1000^2   MB  megabyte
+        // 1000^3   GB  gigabyte
+        // 1000^4   TB  terabyte
+        // 1000^5   PB  petabyte
+        // 1000^6   EB  exabyte
+        // 1000^7   ZB  zettabyte
+        // 1000^8   YB  yottabyte
+
+        return preg_match('/^[kMGTPEZY]B$/ui', $suffix);
+    }
+
+    protected function sanitizeMetricByteUnit(string $suffix): string
+    {
+        if (strtolower($suffix[0]) == 'k') {
+            return "kB";
+        }
+
+        return strtoupper($suffix);
     }
 }
---
+++ tests/Unit/Data/MyStringTest/TestAppend.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+    'hello world!' => [
+        'value' => "hello",
+        'suffix' => " world!",
+        'expected' => "hello world!",
+    ],
+
+    'kilobyte' => [
+        'value' => "10",
+        'suffix' => "kb",
+        'expected' => "10 kB",
+    ],
+
+    'megabyte' => [
+        'value' => "10",
+        'suffix' => "mb",
+        'expected' => "10 MB",
+    ],
+
+    'gigabyte' => [
+        'value' => "10",
+        'suffix' => "gb",
+        'expected' => "10 GB",
+    ],
+
+    'terabyte' => [
+        'value' => "10",
+        'suffix' => "tb",
+        'expected' => "10 TB",
+    ],
+
+    'petabyte' => [
+        'value' => "10",
+        'suffix' => "pb",
+        'expected' => "10 PB",
+    ],
+
+    'exabyte' => [
+        'value' => "10",
+        'suffix' => "eb",
+        'expected' => "10 EB",
+    ],
+
+    'zettabyte' => [
+        'value' => "10",
+        'suffix' => "zb",
+        'expected' => "10 ZB",
+    ],
+
+    'yottabyte' => [
+        'value' => "10",
+        'suffix' => "yb",
+        'expected' => "10 YB",
+    ],
+];
